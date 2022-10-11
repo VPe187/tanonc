86b022a7fe3024fe875a81d9191d04709753a569
assassino1792 <lisoczki.misi@gmail.com>
2022-10-11 22:08:28 +0200

    családi adatok javítás

--- resources/views/familydataimport.blade.php
+++ resources/views/familydataimport.blade.php
@@ -13,17 +13,20 @@
                      <br /> <br />
                      <h4><p>Kérem válassza ki a feltölteni kívánt csv fájlt, majd kattintson a feltöltés gombra!</p></h4>
                      <br /><br /><br />
-                    <!-- <p class="btn btn-lg btn-primary shadow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-folder-plus" viewBox="0 0 16 16">
-  <path d="m.5 3 .04.87a1.99 1.99 0 0 0-.342 1.311l.637 7A2 2 0 0 0 2.826 14H9v-1H2.826a1 1 0 0 1-.995-.91l-.637-7A1 1 0 0 1 2.19 4h11.62a1 1 0 0 1 .996 1.09L14.54 8h1.005l.256-2.819A2 2 0 0 0 13.81 3H9.828a2 2 0 0 1-1.414-.586l-.828-.828A2 2 0 0 0 6.172 1H2.5a2 2 0 0 0-2 2zm5.672-1a1 1 0 0 1 .707.293L7.586 3H2.19c-.24 0-.47.042-.683.12L1.5 2.98a1 1 0 0 1 1-.98h3.672z"/>
-  <path d="M13.5 10a.5.5 0 0 1 .5.5V12h1.5a.5.5 0 1 1 0 1H14v1.5a.5.5 0 1 1-1 0V13h-1.5a.5.5 0 0 1 0-1H13v-1.5a.5.5 0 0 1 .5-.5z"/>
-</svg> Tallózás</p> 
-
+                   
+                <!--
                 <label for="formFileSm" class="form-label">Small file input example</label>
                 <input class="form-control form-control-sm" id="formFileSm" type="file" /> 
                 <label for="formFileLg" class="form-label">Kérem válassza ki a feltölteni kívánt fájlt</label>-->
 
                     <input class="form-control form-control-lg" id="formFileLg" type="file" />
                     <br /><br />
+
+                    <p class="btn btn-lg btn-primary shadow"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-cloud-upload" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M4.406 1.342A5.53 5.53 0 0 1 8 0c2.69 0 4.923 2 5.166 4.579C14.758 4.804 16 6.137 16 7.773 16 9.569 14.502 11 12.687 11H10a.5.5 0 0 1 0-1h2.688C13.979 10 15 8.988 15 7.773c0-1.216-1.02-2.228-2.313-2.228h-.5v-.5C12.188 2.825 10.328 1 8 1a4.53 4.53 0 0 0-2.941 1.1c-.757.652-1.153 1.438-1.153 2.055v.448l-.445.049C2.064 4.805 1 5.952 1 7.318 1 8.785 2.23 10 3.781 10H6a.5.5 0 0 1 0 1H3.781C1.708 11 0 9.366 0 7.318c0-1.763 1.266-3.223 2.942-3.593.143-.863.698-1.723 1.464-2.383z"/>
+  <path fill-rule="evenodd" d="M7.646 4.146a.5.5 0 0 1 .708 0l3 3a.5.5 0 0 1-.708.708L8.5 5.707V14.5a.5.5 0 0 1-1 0V5.707L5.354 7.854a.5.5 0 1 1-.708-.708l3-3z"/>
+</svg> Feltöltés</p>
+ 
                 </div>
             </div>
         </div>
