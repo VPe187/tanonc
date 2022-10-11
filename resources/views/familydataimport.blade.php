db3553705d81d1ada5e2072cf935969f91037830
assassino1792 <lisoczki.misi@gmail.com>
2022-10-11 21:04:52 +0200

    Update familydataimport.blade.php

--- resources/views/familydataimport.blade.php
+++ resources/views/familydataimport.blade.php
@@ -1,1 +1,18 @@
-teszt
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card shadow">
+                <div class="card-header"><i class="fa-btn fa-solid fa-table-columns"></i>Családi adatok importálása</div>
+                <div class="card-body">
+                    <p class="text-center"><img src="img/tanonc_logo.png" /></p>
+                     <p>Kérem válassza ki a feltölteni kívánt fájlt majd kattintson a feltöltés gombra!</p>
+                     
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
