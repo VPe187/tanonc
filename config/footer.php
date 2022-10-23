91e86eb01f728e7a093e93cc74a6cb1e6476f98c
Ateszpti <ptinyefiok@gmail.com>
2022-10-23 17:24:26 +0200

    Footer
    footer.php

---
+++ config/footer.php
@@ -0,0 +1,66 @@
+<!--Akik dolgoztak rajta: 
+	Kód: Hudák Attila
+	Design: Tóth Tímea
+-->
+
+<!DOCTYPE html> <!-- HTML deklaráció-->
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+	
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+	
+	<script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+
+</head>
+<html lang = "hu">
+    <body>
+
+	<div class="mt-5 p-4 bg-dark text-white text-center">
+    <div class="row">
+        <div class="col-sm-4">
+            <h4>Elérhetőségek</h4>
+			<p style="font-size:30;" colspan = "7"><i class = "fa fa-phone"> 06-10/123-123</i></p>
+			<p style="font-size:30;" colspan = "7"><i class = "fa fa-at"> business@probamail.com</i></p>
+		    <p style="font-size:30;" colspan = "7"><i class = "fa fa-envelope"> Szed Elek utca, 34.</i></p>
+        </div>
+        <div class="col-sm-4">
+            <h4>Hírlevél</h4>
+			<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+			   Pellentesque vitae lacus neque. 
+			   Nullam pretium mollis lectus, vel dictum ante congue in. 
+			   In at elementum ex
+			   <form action="" method = "POST">
+        
+				<input type="text" id="name" name="name" placeholder="Keresztnév" 
+					   style = "width:200px;opacity:0.5;border:solid black 1px;font-style:italic">
+		
+				<input type="text" id="email" name="email" placeholder="E-mail cím" 
+					   style = "width:200px;opacity:0.5;border:solid black 1px;font-style:italic">
+				</form>
+			<br/>
+			<button style = "padding:10px">Feliratkozom</button>
+			</p>
+        </div>
+		<div class="col-sm-4">
+            <h4>Információk</h4>
+			<p><a href = "privacy_policy.php" style = "color:white; text-decoration:none;">Adatvédelmi nyilatkozat</a></p>
+			<p><a href = "#" style = "color:white; text-decoration:none;">Partnerek</a></p>
+			<p><a href = "https://kiszervezettmarketing.hu/weboldal-keszites/impresszum-weboldal/" style = "color:white; text-decoration:none;">Impresszum</a></p>
+			<p></p>
+        </div>
+    </div>
+	
+	<a href = "https://www.instagram.com/" style = "color:white; text-decoration:none;">
+	<i class="fab fa-instagram" style = "font-size:30px;"></a></i>
+			
+	<a href = "https://www.facebook.com/" style = "color:white; text-decoration:none;">
+	<i class="fab fa-facebook-square" style = "font-size:30px;"></a></i>
+	
+</div>
+
+    </body>
+</html>
