f07a2e2f564de8680fac6b4345d06d12150edff7
root <zsooltj@gmail.com>
2022-11-24 12:44:38 -0500

    fixname

--- public/index.php
+++ public/index.php
@@ -20,6 +20,7 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
     require $maintenance;
 }
 
+
 /*
 |--------------------------------------------------------------------------
 | Register The Auto Loader
@@ -31,7 +32,9 @@ if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php'))
 |
 */
 
+
 require __DIR__.'/../vendor/autoload.php';
+
 
 /*
 |--------------------------------------------------------------------------
@@ -46,7 +49,10 @@ require __DIR__.'/../vendor/autoload.php';
 
 $app = require_once __DIR__.'/../bootstrap/app.php';
 
+
+
 $kernel = $app->make(Kernel::class);
+
 
 $response = $kernel->handle(
     $request = Request::capture()
