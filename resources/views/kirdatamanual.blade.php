0bf1f7c13fa730308fe29dd01fb5b2aef19153c7
ColossalK3nny <colossalk3nny@hotmail.com>
2022-11-24 13:28:40 +0100

    Ha már oktazon bevitele, ne userid kiíratás legyen.

--- resources/views/kirdatamanual.blade.php
+++ resources/views/kirdatamanual.blade.php
@@ -24,7 +24,7 @@
                             <label for="fieldUserId" class="col-4 col-form-label">Oktatási azonosító</label>
                             <div class="col-md-8">
                                 <div class="input-group">
-                                    <input type="text" class="form-control" name="fieldUserId" value="{{ Auth::user()->id }}" readonly>
+                                    <input type="text" class="form-control" name="oktazon">
                                     <span class="input-group-text"><i class="fa-fw fa-solid fa-key"></i></span>
                                 </div>
                             </div>
